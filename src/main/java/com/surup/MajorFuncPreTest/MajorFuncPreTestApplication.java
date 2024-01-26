829a112d8dffb9e58cfd98919f3e6492b2e4bc00
jeonhyoungmin <jhmin7509@gmail.com>
2024-01-26 13:21:55 +0900

    #1 - 프로젝트 초기화 및 gitignore 설정
    gitignore 설정
    * https://www.toptal.com/developers/gitignore/
    - java
    - gradle
    - intellij + all
    - visual studio code
    - windows
    - macOS

---
+++ src/main/java/com/surup/MajorFuncPreTest/MajorFuncPreTestApplication.java
@@ -0,0 +1,13 @@
+package com.surup.MajorFuncPreTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorFuncPreTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorFuncPreTestApplication.class, args);
+	}
+
+}
